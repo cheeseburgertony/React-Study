b47af0c1d623708fd310be23eece2389f40118e1
cheeseburgertony <1821955020@qq.com>
2024-07-23 14:27:23 +0800

    Less编写方式

--- 04-React-CSS方案/code/learn_reactcss/src/index.js
+++ 04-React-CSS方案/code/learn_reactcss/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './01-内联样式的CSS/App';
 // import App from './02-普通的CSS写法/App';
-import App from './03-CSS_Modules/App';
+// import App from './03-CSS_Modules/App';
+import App from './04-Less编写方式/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
