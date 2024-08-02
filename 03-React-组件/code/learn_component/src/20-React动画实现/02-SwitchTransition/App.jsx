7efc9bae385a3cc716f471f4461b21759fb03954
cheeseburgertony <1821955020@qq.com>
2024-08-02 14:24:02 +0800

    chore: Update SwitchTransition component in App.jsx

--- 03-React-组件/code/learn_component/src/20-React动画实现/02-SwitchTransition/App.jsx
+++ 03-React-组件/code/learn_component/src/20-React动画实现/02-SwitchTransition/App.jsx
@@ -16,7 +16,8 @@ export class App extends PureComponent {
       <div>
         {/* mode='out-in' 表示先移出再移入 */}
         {/* 其他属性需要在CSSTransition中进行设置 */}
-        {/* 用于两个组件的切换动画 */}
+        {/* 用于两个组件之间切换(或者用于组件中不同内容的切换) */}
+        {/* key要根据内容变化进行切换,只有key发生变化了才会执行动画 */}
         <SwitchTransition mode='out-in'>
           <CSSTransition
             key={isLogin ? 'exit' : 'login'}
